83682e5b65a1305c0b52ff16e2358e521620e93f
SiljaOwO <50019638+SiljaOwO@users.noreply.github.com>
2020-10-20 16:26:31 +0300

    Update script.js

--- code/script.js
+++ code/script.js
@@ -20,9 +20,11 @@ function addToList() {
     } 
     //If input is not empty, add user's input to the table
     else {
+        //If table doesn't have any rows, it adds new row
         if (table.rows.length == 0) {
             addRow();
-        } 
+        }
+        //If table has one row but it's empty, it deletes that row and then adds new
         else if (table.rows[0].cells.item(1).innerHTML == "" && table.rows.length == 1) {
             table.deleteRow(0);
             addRow();
@@ -32,10 +34,11 @@ function addToList() {
     }
 }
 
+//Adds new row that includes checkbox, user's input and delete-button
 function addRow() {
     var text = document.getElementById('textfield').value;
     var table = document.getElementById('table');
-    
+
     document.getElementById('textfield').style.borderWidth = "2px";
 
     var row = table.insertRow();
@@ -46,24 +49,54 @@ function addRow() {
     cell1.innerHTML = '<input type="checkbox" id="check" onclick="isChecked(this);">';
     cell2.innerHTML = text;
     cell3.innerHTML = '<button id="delete" onclick="deleteRow(this);"><i class="fa fa-trash" aria-hidden="true"></i></button>';
+
+    //Add local storage
+    var i = 0;
+    while (true) {
+        if (localStorage.getItem(i) == null) {
+            localStorage.setItem(i, text);
+            break;
+        } else {
+            i++;
+        }
+    }
 }
 
+//Deletes row
 function deleteRow(r) {
     var table = document.getElementById('table');
     var i = r.parentNode.parentNode.rowIndex;
     table.deleteRow(i);
+
+    //Delete from local storage
+    localStorage.removeItem(i);
+    while (true) {
+        if (localStorage.getItem(i+1) != null) {
+            var x = localStorage.getItem(i+1);
+            localStorage.setItem(i, x);
+            localStorage.removeItem(i+1);
+            i++;
+        } else {
+            break;
+        }
+    }
 }
 
+//Deletes everything
 function deleteAll() {
     var table = document.getElementById('table');
 
     if (table.rows.length > 0) {
         while (true) {
             table.deleteRow(0);
+
+            //Delete from local storage
+            localStorage.clear();
         }
     }
 }
 
+//When checkbox is checked, text is lined through and when not, text has no decoration
 function isChecked(c) {
     var table = document.getElementById('table');
 
@@ -77,7 +110,34 @@ function isChecked(c) {
     }
 }
 
+//When user clicks form, it selects all text
 function selected() {
     var text = document.getElementById('textfield');
     text.select();
+}
+
+
+//Get data from local storage
+function getData() {
+    for (var i = 0; i < localStorage.length; i++) {
+        var x = localStorage.getItem(i);
+        addDataRow(x);
+    }
+    document.getElementById('table').deleteRow(0);
+}
+
+//Make table of data that is in local storage
+function addDataRow(text) {
+    var table = document.getElementById('table');
+
+    document.getElementById('textfield').style.borderWidth = "2px";
+
+    var row = table.insertRow();
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+
+    cell1.innerHTML = '<input type="checkbox" id="check" onclick="isChecked(this);">';
+    cell2.innerHTML = text;
+    cell3.innerHTML = '<button id="delete" onclick="deleteRow(this);"><i class="fa fa-trash" aria-hidden="true"></i></button>';
 }
